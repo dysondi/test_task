4a024cd1acdca1571c3242534863861d69bda4c0
Hryhorii Vydria <53174747+dysondi@users.noreply.github.com>
2023-10-06 14:03:05 +0200

    Update test.sh
    Add check for random order
    and colors for output

--- test.sh
+++ test.sh
@@ -1,40 +1,64 @@
 #!/bin/bash
 
-# just for test if sequence is wrong
-#sequence=(23 22 123 1 5 16 3 22)
+#collors for output text
+GREEN='\033[0;32m'
+RED='\033[0;31m'  
+NC='\033[0m' 
+
+
+
+
+# you can use this line just for test if sequence is wrong
+#sequence=(23 22 22 123 1 5 16 3)
 sequence=(`./numbers.sh`)
 
 unique=1
 in_range=1
+random_order=1
 
-declare -A seen
-
-for number in "${sequence[@]}"; do
-  if [[ "${seen[$number]}" ]]; then
-    unique=0
-    break
-  else
-    seen["$number"]=1
-  fi
-
-  if [[ $number -lt 1 ]]; then
-    in_range=0
-  fi
-
- if [[ $number -gt 10 ]]; then
-    in_range=0
-  fi
-
+# Check if numbers are unique
+for num1 in $sequence; do
+    count=0
+    for num2 in $sequence; do
+        if [ $num1 -eq $num2 ]; then
+            count=$((count + 1))
+        fi
+    done
+    if [ $count -gt 1 ]; then
+        unique=0
+        break
+    fi
 done
 
-if [[ $unique -eq 1 ]]; then
-  echo "The numbers in the sequence are unique."
-else
-  echo "The numbers in the sequence are not unique."
+# Check if numbers are in range 1-10
+for num in $sequence; do
+    if [ $num -lt 1 ] || [ $num -gt 10 ]; then
+        in_range=0
+        break
+    fi
+done
+
+# Check if the sequence is in random order
+sorted_sequence=$(echo "$sequence" | tr " " "\n" | sort -n | tr "\n" " ")
+if [ "$sequence" == "$sorted_sequence" ]; then
+    random_order=0
 fi
 
-if [[ $in_range -eq 1 ]]; then
-  echo "All numbers in the sequence are within the range of 1-10."
+# Print results
+if [ $unique -eq 1 ]; then
+    echo -e "${GREEN}The numbers in the sequence are unique.${NC}"
 else
-  echo "Some numbers in the sequence are not within the range of 1-10."
+    echo -e "${RED}The numbers in the sequence are not unique.${NC}"
+fi
+
+if [ $in_range -eq 1 ]; then
+    echo -e "${GREEN}All numbers in the sequence are in the range of 1-10.${NC}"
+else
+    echo -e "${RED}Some numbers in the sequence are not in the range of 1-10.${NC}"
+fi
+
+if [ $random_order -eq 1 ]; then
+    echo -e "${GREEN}The sequence is in random order.${NC}"
+else
+    echo -e "${RED}The sequence is not in random order.${NC}"
 fi
